f0ee68be98d74152c870255595108ff539597eec
JMoukas <144281540+JMoukas@users.noreply.github.com>
2024-02-10 15:36:04 -0500

    Removed Database handler.py and database code from app.py
    Removed file and code to be uploaded to a seperate branch

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 import requests, os, random, SteamHandler
 from json import dumps
 from flask import Flask, Response, render_template, redirect, request
-import DatabaseHandler
-
 app = Flask(__name__)
 
 Steam = SteamHandler.Steam(os.environ.get('STEAM_KEY', 'Steam_api_key'))
 web_url = os.environ.get('WEB_URL', 'web_url')
-DatabaseHandler.create_tables()
 
 @app.route("/auth")
 def auth_with_steam():
@@ -17,10 +14,11 @@ def auth_with_steam():
 @app.route("/authorize")
 def authorize():
     user_id=str(request.args['openid.identity']).replace('https://steamcommunity.com/openid/id/','')
-    DatabaseHandler.insert_user(request.args['openid.claimed_id'], user_id)
     #print(request.args)
     return render_template("steam_login_receive_redirect.html",redirect_url=f"{web_url}/user/{user_id}",user_id=user_id)
     #return redirect(f"{web_url}/user/{str(request.args['openid.identity']).replace('https://steamcommunity.com/openid/id/','')}")
+
+
 
 
 @app.route('/')
@@ -39,18 +37,13 @@ def friend_list(steamid):
     friends = Steam.get_user_friend_list(steamid)
     if not friends:
         return render_template("examples/friend_list_error_example.html", user=Steam.get_user_summeries([steamid])[steamid])
-    DatabaseHandler.insert_friend_list(steamid, friends)
     friends = list(friends.values())
     return render_template("examples/friend_list_example.html", friends=friends)
 @app.route('/user/<steamid>/games')
 def game_list(steamid):
     games = Steam.get_user_owned_games(steamid)['games']
-
     if not games:
         return render_template("examples/game_list_error_example.html", user=Steam.get_user_summeries([steamid])[steamid])
-    DatabaseHandler.insert_game_data(steamid, games)
     return render_template("examples/game_list_example.html", games=games)
-
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug = True)
